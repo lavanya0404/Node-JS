458ac4b36e01d458c8ee9c8474c0d88256e0ab3c
Lavanya Y M <lavanyamaruthi@0413.gmail.com>
2024-07-17 18:06:43 +0530

    req.methods and req.url used to get url objects

--- HttpServer/index.js
+++ HttpServer/index.js
@@ -1,19 +1,37 @@
 const http = require("http")
 const fs = require("fs")
+const url = require("url")
 const myServer = http.createServer((req, res) => {
-  const log = `\n${Date.now()}: New request: Recieved\n${req.url}`
-
+  if (req.url == "/favicon.ico") return res.end()
+  const log = `${Date.now()}: New request Recieved\n requested URL:${
+    req.url
+  }\n Requested method : ${req.method}\n`
+  const myURL = url.parse(req.url, true)
   fs.appendFile("log.txt", log, (err, data) => {
-    switch (req.url) {
+    switch (myURL.pathname) {
       case "/":
         res.end("Home Page")
         break
       case "/about":
-        res.end("About Page\n i am lavanya y m")
+        const userName = myURL.query.myName
+        res.end(`Hi,${userName}`)
         break
       case "/contact":
         res.end("Contact Page")
         break
+      case "/search":
+        const search_query = myURL.query.search_query
+        res.end(`Search Results for: ${search_query}`)
+        break
+      case '/signup':
+        if(req.method =="GET"){
+          res.end("This is sign up form")
+        }
+        else if(req.method==='POST'){
+          res.end("Successfully registered")
+        }
+        break
+      case "/error":
       default:
         res.end("Invalid URL")
     }
